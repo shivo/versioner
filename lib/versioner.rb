8be368012f9e5cfdf17c05379dd04b5423a417aa
Joshua Rieken <joshua@joshuarieken.com>
2012-10-15 11:30:47 -0500

    Basic API with specs (no persistence)

--- lib/versioner.rb
+++ lib/versioner.rb
@@ -1,5 +1,17 @@
 require "versioner/versioner_api"
 
 module Versioner
-  
+  @api = VersionerApi.new
+
+  def self.version
+    @api.version
+  end
+
+  def self.force(version)
+    @api.force version
+  end
+
+  def self.increment_major
+    @api.increment_major
+  end
 end
--- spec/lib/versioner_spec.rb
+++ spec/lib/versioner_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe Versioner do
-  it "should set the current version" do
-    
+  it "should report a default version if none is set" do
+    version = Versioner.version
+    version.should == "0.0.0"
+  end
+
+  it "should set the current version via force" do
+    Versioner.force "0.0.1"
+    version = Versioner.version
+    version.should == "0.0.1"
+  end
+
+  it "should increment the major version" do
+    Versioner.force "0.0.1"
+    Versioner.increment_major
+    version = Versioner.version
+    version.should == "1.0.1"
   end
 end
